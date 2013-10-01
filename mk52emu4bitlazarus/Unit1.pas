128eed11eed6318d8a1cdb97d3d811d94a9eb275
a.grabovets <a.grabovets@gmail.com>
2013-10-01 19:14:01 +0000

    safe mode 61/34 switch ver 2.0, 315->210

--- mk52emu4bitlazarus/Unit1.pas
+++ mk52emu4bitlazarus/Unit1.pas
@@ -117,25 +117,20 @@ type
     procedure RadioButton1Click(Sender: TObject);
     procedure RadioButton2Click(Sender: TObject);
     procedure RadioButton3Click(Sender: TObject);
-    procedure FormKeyDown(Sender: TObject; var Key: Word;
-      Shift: TShiftState);
+    procedure FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
     procedure Button36Click(Sender: TObject);
     procedure Button37Click(Sender: TObject);
-
-    procedure safe();
-
+    procedure safe(n: integer);
 
   private
     { Private declarations }
   public
     { Public declarations }
     ind, ind_old: array[0..12] of byte;
-
     ind_multipoint: array[0..13] of boolean;
-
     mk52: Tmk52;
-
     speed: integer;
+    Data: array [0..500] of byte;
   end;
 
 var
@@ -146,25 +141,26 @@ implementation
 
 {$R *.lfm}
 
-procedure TForm1.safe();
+procedure TForm1.safe(n: integer);
 var
   i, start: integer;
 begin
   repeat
-      repeat
-        mk52.exec;
-      until mk52.ik1302.micro_tact = 0;
-      start := 0;
-      for i := 0 to 13 do start := start + mk52.ik1302.read_m((i * 3) + 1);
-    until start = 135;
+    repeat
+      mk52.exec;
+    until mk52.ik1302.micro_tact = 0;
+    start := 0;
+    for i := 0 to 13 do
+      start := start + mk52.ik1302.read_m((i * 3) + 1);
+  until start = 135;
 
-   (* for i := 0 to 0 do
-    begin
-      repeat
-        mk52.exec;
-      until mk52.ik1302.micro_tact = 0;
-    end;
-    *)
+  for i := 0 to n do
+  begin
+    repeat
+      mk52.exec;
+    until mk52.ik1302.micro_tact = 0;
+  end;
+
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
@@ -175,14 +171,14 @@ begin
 
   mk52.ik1303.write_ri(36, 0); // start 00
   mk52.ik1303.write_ri(39, 0);
-//  mk52.init;
+  //  mk52.init;
 
 
 
-//mk52.ik1302.Registr_M[0] := true;
-//mk52.ik1302.Registr_M[1] := true;
-//mk52.ik1302.Registr_M[2] := true;
-//mk52.ik1302.Registr_M[3] := true;
+  //mk52.ik1302.Registr_M[0] := true;
+  //mk52.ik1302.Registr_M[1] := true;
+  //mk52.ik1302.Registr_M[2] := true;
+  //mk52.ik1302.Registr_M[3] := true;
 
 
   (*
@@ -194,24 +190,69 @@ begin
   Indicator1.Height := 50;
   Indicator1.show_ind();
    *)
-  Timer1.Enabled := true;
+  Timer1.Enabled := True;
   ind_old[1] := 1;
   show_ind;
 
-   speed := 1;
+  speed := 1;
 end;
 
 
 procedure TForm1.RadioButton4Click(Sender: TObject);
+var
+  i: integer;
 begin
-   safe();
-   mk52.mode := 1;
+  safe(5);
+  for i := 1 to 196 do
+  begin
+    mk52.exec;
+    Data[i * 2 - 2] := mk52.ir2_2.Rg_Out;
+    mk52.exec;
+    mk52.exec;
+    Data[i * 2 - 1] := mk52.ir2_2.Rg_Out;
+  end;
+
+  safe(0);
+  mk52.mode := 1; // 61
+
+  safe(5);
+  for i := 1 to 210 do
+  begin
+    mk52.exec;
+    mk52.pre_exec(Data[i * 2 - 2]);
+    mk52.exec;
+    mk52.exec;
+    mk52.pre_exec(Data[i * 2 - 1]);
+  end;
+
 end;
 
 procedure TForm1.RadioButton5Click(Sender: TObject);
+var
+  i: integer;
 begin
-   safe();
-   mk52.mode := 2;
+  safe(5);
+  for i := 1 to 210 do
+  begin
+    mk52.exec;
+    Data[i * 2 - 2] := mk52.ir2_2.Rg_Out;
+    mk52.exec;
+    mk52.exec;
+    Data[i * 2 - 1] := mk52.ir2_2.Rg_Out;
+  end;
+
+  safe(0);
+  mk52.mode := 2; // 34
+
+  safe(5);
+  for i := 1 to 196 do
+  begin
+    mk52.exec;
+    mk52.pre_exec(Data[i * 2 - 2]);
+    mk52.exec;
+    mk52.exec;
+    mk52.pre_exec(Data[i * 2 - 1]);
+  end;
 end;
 
 procedure TForm1.RadioButton6Click(Sender: TObject);
@@ -221,12 +262,12 @@ end;
 
 procedure TForm1.RadioButton7Click(Sender: TObject);
 begin
-   speed := 10;
+  speed := 10;
 end;
 
 procedure TForm1.RadioButton8Click(Sender: TObject);
 begin
-   speed := 100;
+  speed := 100;
 end;
 
 procedure TForm1.show_ind();
@@ -237,23 +278,24 @@ var
   x, y, dig, i, step: integer;
   update_: boolean;
 begin
-  update_ := false;
+  update_ := False;
   for i := 0 to 12 do
   begin
-    if ind_old[i] <> ind[i] then update_ := true;
+    if ind_old[i] <> ind[i] then
+      update_ := True;
     ind_old[i] := ind[i];
   end;
   if update_ then
   begin
- /////////////////////////
+    /////////////////////////
     bmp := tbitmap.Create;
     bmp.Width := 220;
     bmp.Height := 50;
 
- // bmp.Transparent := true;
+    // bmp.Transparent := true;
     bmp.TransparentColor := clwhite;
 
- // bmp.Canvas.Brush.Color := clgreen;
+    // bmp.Canvas.Brush.Color := clgreen;
     bmp.Canvas.Pen.Style := psSolid;
     bmp.Canvas.Pen.Width := 3;
     bmp.Canvas.Pen.Color := $E0FF00;
@@ -261,7 +303,8 @@ begin
 
     for i := 0 to 11 do
     begin
-      x := 5 + (i * 16); y := 5;
+      x := 5 + (i * 16);
+      y := 5;
       case ind[i] of
         10: dig := 64;
         0: dig := 63;
@@ -274,30 +317,62 @@ begin
         7: dig := 7;
         8: dig := 127;
         9: dig := 111;
-    //if ind[i + 1] = 'A' then dig := 64;
+        //if ind[i + 1] = 'A' then dig := 64;
         11: dig := 56;
         12: dig := 57;
         13: dig := 49;
         14: dig := 121;
-       // 15: dig := 113;
+        // 15: dig := 113;
 
         15: dig := 0;
 
-    //if ind[i + 1] = ' ' then dig := 0;
+        //if ind[i + 1] = ' ' then dig := 0;
       end;
-      if (dig and 1) > 0 then begin bmp.Canvas.MoveTo(x + 0, y + 0); bmp.Canvas.LineTo(x + 10, y + 0); end; //a
-      if (dig and 2) > 0 then begin bmp.Canvas.MoveTo(x + 10, y + 0); bmp.Canvas.LineTo(x + 10, y + 10); end; //b
-      if (dig and 4) > 0 then begin bmp.Canvas.MoveTo(x + 10, y + 10); bmp.Canvas.LineTo(x + 10, y + 20); end; //c
-      if (dig and 8) > 0 then begin bmp.Canvas.MoveTo(x + 10, y + 20); bmp.Canvas.LineTo(x + 0, y + 20); end; //d
-      if (dig and 16) > 0 then begin bmp.Canvas.MoveTo(x + 0, y + 20); bmp.Canvas.LineTo(x + 0, y + 10); end; //e
-      if (dig and 32) > 0 then begin bmp.Canvas.MoveTo(x + 0, y + 10); bmp.Canvas.LineTo(x + 0, y + 00); end; //f
-      if (dig and 64) > 0 then begin bmp.Canvas.MoveTo(x + 0, y + 10); bmp.Canvas.LineTo(x + 10, y + 10); end; //g
-  //    if ((ind[12] + 1) = i) and (i <> 9) then begin bmp.Canvas.MoveTo(x + 12, y + 22); bmp.Canvas.LineTo(x + 12, y + 23); end; //g
+      if (dig and 1) > 0 then
+      begin
+        bmp.Canvas.MoveTo(x + 0, y + 0);
+        bmp.Canvas.LineTo(x + 10, y + 0);
+      end; //a
+      if (dig and 2) > 0 then
+      begin
+        bmp.Canvas.MoveTo(x + 10, y + 0);
+        bmp.Canvas.LineTo(x + 10, y + 10);
+      end; //b
+      if (dig and 4) > 0 then
+      begin
+        bmp.Canvas.MoveTo(x + 10, y + 10);
+        bmp.Canvas.LineTo(x + 10, y + 20);
+      end; //c
+      if (dig and 8) > 0 then
+      begin
+        bmp.Canvas.MoveTo(x + 10, y + 20);
+        bmp.Canvas.LineTo(x + 0, y + 20);
+      end; //d
+      if (dig and 16) > 0 then
+      begin
+        bmp.Canvas.MoveTo(x + 0, y + 20);
+        bmp.Canvas.LineTo(x + 0, y + 10);
+      end; //e
+      if (dig and 32) > 0 then
+      begin
+        bmp.Canvas.MoveTo(x + 0, y + 10);
+        bmp.Canvas.LineTo(x + 0, y + 00);
+      end; //f
+      if (dig and 64) > 0 then
+      begin
+        bmp.Canvas.MoveTo(x + 0, y + 10);
+        bmp.Canvas.LineTo(x + 10, y + 10);
+      end; //g
+      //    if ((ind[12] + 1) = i) and (i <> 9) then begin bmp.Canvas.MoveTo(x + 12, y + 22); bmp.Canvas.LineTo(x + 12, y + 23); end; //g
 
-      if ind_multipoint[i] then begin bmp.Canvas.MoveTo(x + 12, y + 22); bmp.Canvas.LineTo(x + 12, y + 23); end; //g
+      if ind_multipoint[i] then
+      begin
+        bmp.Canvas.MoveTo(x + 12, y + 22);
+        bmp.Canvas.LineTo(x + 12, y + 23);
+      end; //g
 
     end;
-//    Image1.Transparent := true;
+    //    Image1.Transparent := true;
     Image1.Picture.Bitmap.Assign(bmp);
     bmp.Free;
   end;
@@ -330,41 +405,48 @@ begin
   mk52.ik1303.keyboard_y := 1;
   mk52.ik1303.keyboard_x := mk52.grd + 9;
 
-  twinkle := true;
+  twinkle := True;
   for i2 := 1 to 56 * speed do // 10x 560 // реальная скорость (560 комманд/сек)
   begin
-    for i := 0 to 41 do mk52.exec;
+    for i := 0 to 41 do
+      mk52.exec;
     //if ((mk52.ik1302.sk = $35) or (mk52.ik1302.sk = $09)) and twinkle and (mk52.ik1302.micro_tact = 164) then
     if mk52.ik1302.ind then
     begin
-      for i := 0 to 8 do ind[i] := mk52.ik1302.read_ri((8 - i) * 3);
-      for i := 0 to 2 do ind[i + 9] := mk52.ik1302.read_ri((11 - i) * 3);
+      for i := 0 to 8 do
+        ind[i] := mk52.ik1302.read_ri((8 - i) * 3);
+      for i := 0 to 2 do
+        ind[i + 9] := mk52.ik1302.read_ri((11 - i) * 3);
       ind[12] := 8 - mk52.ik1302.segment_i8;
 
-      for i := 0 to 8 do ind_multipoint[i] := mk52.ik1302.ind_multipoint[9 - i];
-      for i := 0 to 2 do ind_multipoint[i + 9] := mk52.ik1302.ind_multipoint[12 - i];
+      for i := 0 to 8 do
+        ind_multipoint[i] := mk52.ik1302.ind_multipoint[9 - i];
+      for i := 0 to 2 do
+        ind_multipoint[i + 9] := mk52.ik1302.ind_multipoint[12 - i];
 
 
-      mk52.ik1302.ind := false;
-      twinkle := false;
+      mk52.ik1302.ind := False;
+      twinkle := False;
       //memo1.Lines.Add('SK := ' + inttohex(mk52.ik1302.sk, 2));
     end;
 
-//      show_ind;
+    //      show_ind;
   end;
- // for i := 0 to 11 do
- // begin
- //   ind[i] := mk52.ik1302.read_ri(i * 3);
- //   ind[12] := mk52.ik1302.segment_i8;
- // end;
+  // for i := 0 to 11 do
+  // begin
+  //   ind[i] := mk52.ik1302.read_ri(i * 3);
+  //   ind[12] := mk52.ik1302.segment_i8;
+  // end;
 
- // if twinkle then show_ind else off_ind;
+  // if twinkle then show_ind else off_ind;
 
   if twinkle then // тьма
   begin
-    for i := 0 to 11 do ind[i] := 15;
+    for i := 0 to 11 do
+      ind[i] := 15;
     ind[12] := 8;
-    for i := 0 to 11 do ind_multipoint[i] := false;
+    for i := 0 to 11 do
+      ind_multipoint[i] := False;
   end
   else
   begin
@@ -383,7 +465,7 @@ var
   alfa: boolean;
   ind_str: string;
 begin
-//  for i := 0 to 3 do
+  //  for i := 0 to 3 do
   begin
     // if (ik1302.micro_tact div 12) = 11 then ik1302.h := 8 else ik1302.h := 0;
     // mk52.ik1302.h := 8+1;
@@ -397,12 +479,12 @@ begin
   memo1.Lines.Add('asp := ' + inttohex(mk52.ik1302.asp, 2));
   memo1.Lines.Add('adr mc := ' + inttohex(mk52.ik1302.adr_mc, 2));
 
-  memo1.Lines.Add('S := ' + inttostr(mk52.ik1302.read_rs(0)));
-  memo1.Lines.Add('S1 := ' + inttostr(mk52.ik1302.read_rs(1)));
-  memo1.Lines.Add('L := ' + inttostr(byte(mk52.ik1302.l)));
+  memo1.Lines.Add('S := ' + IntToStr(mk52.ik1302.read_rs(0)));
+  memo1.Lines.Add('S1 := ' + IntToStr(mk52.ik1302.read_rs(1)));
+  memo1.Lines.Add('L := ' + IntToStr(byte(mk52.ik1302.l)));
   memo1.Lines.Add('SK := ' + inttohex(mk52.ik1302.sk, 2));
   ///
-  memo1.Lines.Add('micro_tact := ' + inttostr(byte(mk52.ik1302.micro_tact)));
+  memo1.Lines.Add('micro_tact := ' + IntToStr(byte(mk52.ik1302.micro_tact)));
 
   StringGrid1.Cells[1, 1] := '1';
 
@@ -427,8 +509,8 @@ begin
   for i := 0 to 11 do
   begin
     ind_str := ind_str + inttohex(mk52.ik1302.read_ri(i * 3), 1);
-  //  ind[i] := mk52.ik1302.read_ri(i * 3);
-  //  ind[12] := mk52.ik1302.segment_i8;
+    //  ind[i] := mk52.ik1302.read_ri(i * 3);
+    //  ind[12] := mk52.ik1302.segment_i8;
   end;
   edit1.Text := ind_str;
 
@@ -455,24 +537,25 @@ begin
   //edit1.Text := inttostr(byte(true));
   //dit1.Text := inttostr(not 1);
 
-
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 var
   key, key_x, key_y: integer;
 begin
-//
-  key := strtoint(copy(TButton(Sender).Name, 7, 2)) - 1;
+
+  key := StrToInt(copy(TButton(Sender).Name, 7, 2)) - 1;
   key_y := (key div 10) + 1;
-  if key_y = 2 then key_y := 8;
-  if key_y = 3 then key_y := 9;
+  if key_y = 2 then
+    key_y := 8;
+  if key_y = 3 then
+    key_y := 9;
   key_x := (key mod 10) + 2;
 
   mk52.ik1302.keyboard_y := key_y;
   mk52.ik1302.keyboard_x := key_x;
 
-  edit1.Text := inttostr(key_y) + ' ' + inttostr(key_x);
+  edit1.Text := IntToStr(key_y) + ' ' + IntToStr(key_x);
 
 end;
 
@@ -486,8 +569,9 @@ procedure TForm1.Button33Click(Sender: TObject);
 var
   i: integer;
 begin
-  Timer1.Enabled := false;
-  for i := 0 to 41 do Button31Click(sender);
+  Timer1.Enabled := False;
+  for i := 0 to 41 do
+    Button31Click(Sender);
 
 end;
 
@@ -503,14 +587,14 @@ begin
   begin
     ir2_1.Rg_In := 1;
     ir2_1.tact_exec2(1);
-    memo1.Lines.Add('ir2_1 := ' + inttostr(i) + ' - ' + inttostr(integer(ir2_1.Rg_Out)));
+    memo1.Lines.Add('ir2_1 := ' + IntToStr(i) + ' - ' + IntToStr(integer(ir2_1.Rg_Out)));
 
   end;
 end;
 
 procedure TForm1.Button35Click(Sender: TObject);
 begin
-  Timer1.Enabled := true;
+  Timer1.Enabled := True;
 end;
 
 procedure TForm1.RadioButton1Click(Sender: TObject);
@@ -531,10 +615,9 @@ begin
   memo1.Lines.Add('3');
 end;
 
-procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
+procedure TForm1.FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
 begin
-  memo1.Lines.Add(inttostr(Key));
+  memo1.Lines.Add(IntToStr(Key));
 
   TButton(Sender).Name := 'Form1';
   case Key of
@@ -564,59 +647,66 @@ begin
     70: TButton(Sender).Name := 'Button30';
   end;
 
-  if pos('Button', TButton(Sender).Name) > 0 then Button1Click(Sender);
+  if pos('Button', TButton(Sender).Name) > 0 then
+    Button1Click(Sender);
 
 end;
 
 procedure TForm1.Button36Click(Sender: TObject);
 var
   ToF: file;
-  data: byte;
+  Data: byte;
   i, start: integer;
   filename: string;
+
+  data1, data2, data3: byte;
 begin
   SaveDialog1.Filter := 'Bin files (*.bin)|*.bin';
   if SaveDialog1.Execute then
   begin
     filename := SaveDialog1.FileName;
-    repeat
-      repeat
-        mk52.exec;
-      until mk52.ik1302.micro_tact = 0;
-      start := 0;
-      for i := 0 to 13 do start := start + mk52.ik1302.read_m((i * 3) + 1);
-    until start = 135;
 
-    for i := 0 to 5 do
-    begin
-      repeat
-        mk52.exec;
-      until mk52.ik1302.micro_tact = 0;
-    end;
-
-
-    if pos('.bin', filename) = 0 then filename := filename + '.bin';
+    if pos('.bin', filename) = 0 then
+      filename := filename + '.bin';
 
     AssignFile(ToF, filename);
     Rewrite(ToF, 1);
 
-    for i := 1 to 315 do
+    safe(5);
+    for i := 1 to 210 do //315
     begin
       mk52.exec;
-      data := mk52.ir2_2.Rg_Out;
+      Data := mk52.ir2_2.Rg_Out;
+
+      mk52.exec; //210
+
       mk52.exec;
-      data := data + (mk52.ir2_2.Rg_Out shl 4);
-      BlockWrite(ToF, data, 1);
+      Data := Data + (mk52.ir2_2.Rg_Out shl 4);
+      BlockWrite(ToF, Data, 1);
     end;
 
     CloseFile(ToF);
   end;
+
+  safe(5);
+  for i := 1 to 210 do
+  begin
+    mk52.exec;
+    data1 := mk52.ir2_2.Rg_Out;
+    mk52.exec;
+    data2 := mk52.ir2_2.Rg_Out;
+    mk52.exec;
+    data3 := mk52.ir2_2.Rg_Out;
+    memo1.Lines.Add(IntToStr(i) + '-' + inttohex(data1, 2) + ',' +
+      inttohex(data2, 2) + ',' + inttohex(data3, 2));
+  end;
+
 end;
 
 procedure TForm1.Button37Click(Sender: TObject);
 var
   FromF: file;
-  data: byte;
+  Data: byte;
   i: integer;
   filename: string;
 begin
@@ -632,21 +722,24 @@ begin
     AssignFile(FromF, filename);
     Reset(FromF, 1);
 
-    if FileSize(FromF) = 315 then
-      for i := 1 to 315 do
+    safe(5);
+
+    if FileSize(FromF) = 210 then
+      for i := 1 to 196 + (2 - mk52.mode) * 14 do
       begin
-        BlockRead(FromF, data, 1);
+        BlockRead(FromF, Data, 1);
 
         mk52.exec;
-        mk52.pre_exec(data and $F);
+        mk52.pre_exec(Data and $F);
+
+        mk52.exec; //210
 
         mk52.exec;
-        mk52.pre_exec(data shr 4);
+        mk52.pre_exec(Data shr 4);
       end;
 
     CloseFile(FromF);
   end;
 end;
 
-end.
-
+end.
